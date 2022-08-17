c0a8f82cb39779e242095a32a5b9d1923275aa12
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-08-17 16:34:25 +0530

    switch Case

---
+++ switchCase.php
@@ -0,0 +1,21 @@
+<?php
+
+$age = 67;
+
+switch($age){
+    case 12:
+        echo "you are 12 years old";
+        break;
+
+        case 45:
+            echo "you are 45 year old";
+            break;
+            
+            case 56:
+
+                echo "You are 56 years old";
+                break;
+                default:
+                echo "You are dead or not born";
+}
+?>
