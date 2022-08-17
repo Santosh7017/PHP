6cb90334b50ba2157a62f1ab1a4acdc7e72a1943
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-08-17 16:33:53 +0530

    dataType

---
+++ dataTypes.php
@@ -0,0 +1,44 @@
+<?php
+
+/* Php datatypes
+
+1. String   
+2. Integer
+3. Float
+4. Boolean
+5. Object
+6. Array
+7. Null
+*/
+//  string - sequence of characters
+
+
+// boolean 
+
+$x = true;
+$is_friend = false;
+
+// echo var_dump($x);
+echo "<br>";
+
+    // $name = "santosh";
+    // echo name-number;
+
+    // object
+
+    // employee is a class ---> santosh can be one object
+
+    //  Array - use to store multiple values in a single variables
+
+    $friends = array("xyz", "abc", "pqrs");
+    echo "<br>";
+    echo var_dump($friends);
+
+
+    $name = NULL;
+echo "<br>";
+    echo var_dump($name);
+
+
+
+?>
