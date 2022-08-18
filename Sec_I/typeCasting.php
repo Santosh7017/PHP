b3a5e93b6d66ab2e8710279749b20716832ca3a3
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-08-18 22:56:49 +0530

    typeCasting

---
+++ Sec_I/typeCasting.php
@@ -0,0 +1,17 @@
+<?php
+// $num = "";
+// $num_two = 8;
+// // $result = (string)($num + $num_two);
+// $result = (boolean)($num);
+// // representation of null in Integer is 0;
+// // for conversion of true into Integer gives 1 and 0 for false
+// //  other than 0 is true either it is integer or any string;
+
+// var_dump($result);
+
+
+$arr = [10,20.0,"3",5,6];
+$arr_two = [10,"akash" => 20, 3,5,6];
+
+var_dump($arr+$arr_two);
+?>
