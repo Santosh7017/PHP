78079af9cc91de6898c70831be8ff8f512c1d815
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-09-09 13:10:09 +0530

    string

---
+++ Sec_I/StringFuntions.php
@@ -0,0 +1,33 @@
+<?php
+
+// echo addcslashes("hello class","as");
+// $str = "don't";
+// echo addslashes("$str it's me ");
+$str = " Hello brilliant students ";
+// echo bin2hex($str);  
+// echo chop($str, "hs");
+
+// echo chop($str,"tsuend ");
+
+// echo chr(76);
+// echo ord("");
+
+// echo chunk_split($str, 4);
+
+// print_r(count_chars($str,1));
+// echo html_entity_decode("&lt;a;/a&gt")
+// echo htmlspecialchars(" hello world; &lt;a;  " );
+// echo convert_uudecode("santosh");
+// echo convert_uudecode("");
+// $var = "hello";
+// $val = convert_uuencode($var);
+// echo $val;
+// $v = convert_uudecode($val);
+// echo $v;
+
+// echo ucfirst("hello");
+// echo strrev("hello  world");
+echo strchr("hello world", "l");
+
+
+?>
