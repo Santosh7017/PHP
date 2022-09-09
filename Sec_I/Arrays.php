7fab36792bccde00acc3818009ac80666949c75b
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-09-09 13:08:42 +0530

    arrays

---
+++ Sec_I/Arrays.php
@@ -0,0 +1,88 @@
+<?php
+// indexed array
+// $arr = [10,34,234,3984];
+// $arr_2 = array(123,234,3);
+// print_r($arr);
+
+//associative array
+// $data = ["ROHIT" => 38, "MOHIT" =>3445, "KLJJF"=> 238];
+// $data_2 = ["ROHIT" => 38];
+
+// echo $data["key_1"];
+// foreach($data as $key => $val){
+//     echo "Name of the key:  $key and its value is $val";
+// }
+
+// print_r(array_change_key_case($data));
+// print_r(array_chunk($data,2))
+// print_r(array_combine($arr,$arr_2));
+
+// print_r(array_diff( $arr,$arr_2));
+
+
+// foreach($data as $key => $val){
+//     echo "Name of the key:  $key and its value is $val". "<br>";
+// }
+// $arr_3 =array_fill(3,4,2
+// print_r($arr_3);
+
+// print_r(array_intersect($arr,$arr_2));
+// print_r(array_intersect_key($data,$data_2));
+// print_r(array_key_exists("ROHI",$data));
+// array_keys($data);
+// array_push($arr);
+// array_pop($arr);
+// print_r($arr);
+// unset($arr[1]);
+// print_r($arr);
+
+// print_r(array_product($arr));
+// print_r(array_replace($arr,$arr_2));
+// array_splice();
+
+// print_r(array_slice($arr,1,2));
+// shuffle($arr);
+// print_r($arr);
+
+// sort($arr);
+// print_r($arr);
+// asort($arr);
+//  $data_one = [2,3,4,5,8,9,92,65];
+//  $new_data = range(min($data_one),max($data_one));
+//  print_r(array_diff($new_data,$data_one));
+
+
+
+// echo "<table style = 'border:2px solid red; border-collapse: collapse'>";
+// for($row = 2;$row <= 10;$row++){
+//     echo "<tr>";
+//     for($column = 1; $column <= 10;$column++){
+        
+//         echo "<td style = 'border:2px solid blue; '>"."$row * $column = ".$row * $column."</td>" ;
+//     }
+//     echo "</tr>";
+// }
+// echo "</table>"
+
+echo "<table style = 'border:2px solid red; border-collapse: collapse'>";
+for($row = 1;$row <= 8;$row++){
+    echo "<tr>";
+    for($column = 1; $column <= 8;$column++){
+        if(($column)%2 == 0){
+            
+        echo "<td style = 'background:black; height:100px; width:100px'>"."</td>" ;
+    }else{
+        echo "<td style = 'background:white; height:100px; width:100px'>"."</td>" ;
+    }
+    }
+    echo "</tr>";
+}
+echo "</table>"
+
+
+
+
+
+
+
+?>
