2ce6b3d33b16414028650ad15de6570d12830391
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-09-15 08:34:27 +0530

    changes

---
+++ Sec_I/loops.php
@@ -0,0 +1,35 @@
+<?php
+// while Loop
+
+// for($i = 0;$i<5;$i++){
+//     echo "$i <br>";
+// }
+
+
+
+// while loop
+// $a = 4;
+
+// while($a>0){
+//     echo $a . "<br>";
+//     $a--;
+// }
+
+
+// do while loop
+// $a = 5;
+// do{
+//     echo "this is do while loop <br>";
+//     $a--;
+// }while($a>0);
+
+// for each loop
+
+$fruits = array("mango", "apple", "Banana", "Orange", "Grapes");
+foreach($fruits as $value){
+    echo $value . "<br>";
+
+}
+
+
+?>
--- Sec_I/table.php
+++ Sec_I/table.php
@@ -1,29 +1,69 @@
 <?php
-echo "<table style='border:2px solid red;border-collapse:collapse'>";
-for($row = 0;$row<10;$row++){
-    echo "<tr>";
-    for($column = 0;$column<10;$column++){
-        echo $row.$column. " ";
-    }
-    echo "<br>";
-}
+
+// for($row = 0;$row<10;$row++){
+
+//     for($column = 0;$column<10;$column++){
+//         echo $row.$column. " ";
+//     }
+//     echo "<br>";
+// }
+
+// echo "<table style='border:2px solid red; border-collapse:collapse'>";
+// for($row = 0;$row<10;$row++){
+//     echo "<tr>";
+//     for($column = 0;$column<10;$column++){
+//         echo "<td style ='border:2px; border:2px solid black'>".$row.$column;
+       
+//     }
+//     echo "<br>";
+// }
+// echo "</table>";
+
+
+// $counter = 0;
+// echo "<table style ='border:2px solid red;border-collapse:collapse'>";
+// for($row = 1;$row <=10;$row++){
+//     echo "<tr>";
+//     for($col = 1;$col <=10;$col++){
+//         echo "<td style = 'border:2px solid black'>".$counter."</td>";
+//         $counter++;
+//     }
+
+// }
+// echo "</table>";
 
 // echo "Hello world";
 
-echo "<table style = 'border:2px solid red; border-collapse: collapse'>";
-for($row = 1;$row <= 8;$row++){
-    echo "<tr>";
-    for($column = 1; $column <= 8;$column++){
-        if(($column)%2 == 0){
+// echo "<table style = 'border:2px solid red; border-collapse: collapse'>";
+// for($row = 1;$row <= 8;$row++){
+//     echo "<tr>";
+//     for($column = 1; $column <= 8;$column++){
+//         if(($row + $column)%2 == 0){
             
-        echo "<td style = 'background:black; height:100px; width:100px'>"."</td>" ;
-    }else{
-        echo "<td style = 'background:white; height:100px; width:100px'>"."</td>" ;
-    }
-    }
-    echo "</tr>";
-}
-echo "</table>"
+//         echo "<td style = 'background:black; height:100px; width:100px'>"."</td>" ;
+//     }else{
+//         echo "<td style = 'background:white; height:100px; width:100px'>"."</td>" ;
+//     }
+//     }
+//     echo "</tr>";
+// }
+// echo "</table>"
+
+
+// echo "<table style = 'border:2px solid red; border-collapse: collapse'>";
+// for($row = 1;$row <= 8;$row++){
+//     echo "<tr>";
+//     for($column = 1; $column <= 8;$column++){
+//         if(($column)%2 == 0){
+            
+//         echo "<td style = 'background:black; height:100px; width:100px'>"."</td>" ;
+//     }else{
+//         echo "<td style = 'background:white; height:100px; width:100px'>"."</td>" ;
+//     }
+//     }
+//     echo "</tr>";
+// }
+// echo "</table>"
 
 
 
