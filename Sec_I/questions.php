7e93514cd9168885a957bc514d0f1b03fea9bf09
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-10-04 16:26:12 +0530

    questions

---
+++ Sec_I/questions.php
@@ -0,0 +1,17 @@
+<?php
+
+// $str = $_POST['string'];
+
+// echo chunk_split($str, 3);
+$num_one = $_POST['num_one'];
+$num_two = $_POST['num_two'];
+$num_three = $_POST['num_3'];
+
+
+
+// $max = $num_one > $num_two ? $num_one : $num_two;
+// echo $max > $num_three?$max:$num_three;
+
+
+
+?>
