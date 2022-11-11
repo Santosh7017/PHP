457c1422dee271d70dc91aca84d04c2a486eae49
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-11-11 13:46:01 +0530

    pseudo code

---
+++ Sec_I/Object oriented programming/pseudo.php
@@ -0,0 +1,22 @@
+<?php
+class gla{
+    public $data = 24;
+    private $num = 38;
+    private function task(){
+        echo "Hello";
+    }
+    private function display(){
+        $this->task();
+    }
+}
+$class = new gla();
+// how to call private function directly by object
+$obj = new ReflectionMethod('gla','task');
+$obj->setAccessible(true);
+$obj->invoke(new gla);
+
+
+$newobj = new ReflectionProperty('gla','task');
+$newobj->setAccessible(true);
+$newobj-> getValue(new gla);
+?>
