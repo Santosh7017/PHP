ed40845305524892ef6303989f3b1b5b56085e54
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-11-17 08:12:39 +0530

    file Handling

--- Sec_I/fileHandling/filehandling.php
+++ Sec_I/fileHandling/filehandling.php
@@ -1,6 +1,27 @@
  <?php
 
-$var = fopen("add.php","w");
-fwrite($var, "hello");
+// $var = fopen("add.php","w");
+// fwrite($var, "hello");
+
+// $var = fopen("sec-i","w");
+// fwrite($var, "hello /n class");
+
+// $open = fopen("sec-i","r");
+// $size = filesize("sec-i")  // return value in bytes
+// //  $var2 = fread($open, $size);
+// // echo "fgets("sec-i")";
+// while(feof("sec-i")){
+
+// }
+
+$var = fopen("sec-i","w+");
+while(!feof($var)){
+        echo fgets($var);
+}
+// unlink("sec-i"); // delete file;
+// echo file_exists("sec-i");
+// fclose($var);
+echo $_SERVER("sec-i");
+
 
 ?>
