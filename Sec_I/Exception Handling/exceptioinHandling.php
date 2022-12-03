3a0ba6983b296072a012d1156fbcffe93fe48d69
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-12-04 00:21:33 +0530

    exception handling

---
+++ Sec_I/Exception Handling/exceptioinHandling.php
@@ -0,0 +1,33 @@
+<!-- $_SERVER show the current locatoin -->
+<?php
+error_reporting(0);
+echo "<form action='$_SERVER[PHP_SELF]' method='post'>
+Enter Value of numerator <input type='number' name='numerator'></input>
+Enter the value of Denominator <input type='number' name='denominator'>
+<input type='submit' name='result' value='output'><br></form>";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    // echo "HELLO ";
+
+    $num = $_POST['numerator'];
+    $den = $_POST['denominator'];
+    class DivisionByzeroo extends Exception{
+        function errorMessage(){
+            return $this -> getMessage();
+        }
+    }
+    try{
+    if($den == 0){
+        // throw new Exception("Provide a valid number except 0");
+        throw new   DivisionByzeroo("Provide a valid number except 0");
+    }
+    // }catch(Exception $eg){
+    }catch(DivisionByzeroo $eg){
+        echo $eg -> getMessage();
+    }
+}
+
+
+
+
+?>
